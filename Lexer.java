a312faf22dfdf2a6198fdc016924fe3e5710931b
GrantLarsen314 <54328249+GrantLarsen314@users.noreply.github.com>
2019-09-13 14:33:34 -0500

    Update Lexer.java

--- Lexer.java
+++ Lexer.java
@@ -1,29 +1,7 @@
+/*±≠≠≠±≠±≠±≠±≠±≠±±≠±≠≠±≠±≠±≠±≠±≠±≠±≠±≠±≠≠±≠±≠±≠±≠±≠±≠±≠±≠±≠±≠±±±≠±≠≠≠≠≠±≠±≠±≠±≠±≠±≠±≠±≠±≠±
+Title: Lexer
 
+Authors: Ethan Jett and Grant Larsen
 
-
-
-
-
-
-
-
-
-import java.util.*;
-
-public class Lexer {
-
-  
-  public static String[] lexing {
-    
-  Scanner input = new Scanner(System.in);
-  String purple = input.nextLine();
-  String file = new String(Files.readAllBytes(Paths.get(purple)));
-  
-  
-  
-  }
-
-
-
-
-}
+Description: Our futal attempt at creating a Lexer for Lazett
+≠±≠≠≠±≠≠±≠±≠±≠±≠±≠±≠±±≠±≠≠±≠±±≠±±≠±±≠±≠±±≠±±±≠±≠±≠≠±≠±≠±≠±≠±≠±≠±±±≠±≠±≠±±±±≠±≠±±±±≠±≠±≠±≠*/
